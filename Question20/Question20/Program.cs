9e5a6544fdd864bfbb8935f721a9e694fe1f43f9
Sachin Vaid <vaid.mca@gmail.com>
2025-01-14 21:37:17 -0500

    CommittedCommit

---
+++ Question20/Question20/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    
+    public static Func<int> GenerateRandomNumber = () =>
+    {
+        Random rand = new Random();
+        return rand.Next(1, 10001);  
+    };
+
+   
+    public static Func<Func<int>, string> GenerateMessage = (generateNumberFunc) =>
+    {
+        int number = generateNumberFunc();  
+        return $"The generated number is: {number}";
+    };
+
+    static void Main(string[] args)
+    {
+        
+        TaskFactory taskFactory = new TaskFactory();
+
+        
+        taskFactory.StartNew(() =>
+        {
+            return GenerateRandomNumber(); 
+        })
+        .ContinueWith(task =>
+        {          
+            string result = GenerateMessage(GenerateRandomNumber);
+            Console.WriteLine(result);  
+        })
+        .Wait();  
+               
+        Console.ReadLine();
+    }
+}
